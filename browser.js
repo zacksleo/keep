7ea5736255d0e426f4b4f2aab3f4a1e45f7f9345
Andrey Polischuk <andre.polischuk@gmail.com>
2016-10-25 19:03:39 +0300

    Remove unused styles

--- browser.js
+++ browser.js
@@ -23,28 +23,6 @@ function handleDOMLoaded() {
   `, 0);
 
   document.styleSheets[0].insertRule(`
-    .notes-container {
-      padding-top: 0 !important;
-    }
-  `, 0);
-
-  document.styleSheets[0].insertRule(`
-    .search-container {
-      position: absolute;
-      top: 7px;
-      left: 165px;
-      right: 125px;
-    }
-  `, 0);
-
-  document.styleSheets[0].insertRule(`
-    .color-container {
-      margin-left: 60px;
-      position: relative;
-    }
-  `, 0);
-
-  document.styleSheets[0].insertRule(`
     ::-webkit-scrollbar {
       display: none !important;
     }
